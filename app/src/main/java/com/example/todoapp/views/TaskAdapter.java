bca8d48c9d1f3aa4ea59c4bec6adacab42a7b90a
Красноперов Михаил <muxajlu-4@yandex.ru>
2021-04-21 21:04:14 +0400

    Add OnClickListener in TaskAdapter.java

--- app/src/main/java/com/example/todoapp/views/TaskAdapter.java
+++ app/src/main/java/com/example/todoapp/views/TaskAdapter.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -17,19 +18,17 @@ import com.example.todoapp.models.Task;
 
 import java.util.List;
 
-public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder>{
-
-    private final LayoutInflater inflater;
+public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
     private final List<Task> tasks;
 
     TaskAdapter(Context context, List<Task> task) {
         this.tasks = task;
-        this.inflater = LayoutInflater.from(context);
     }
+
     @Override
     public TaskAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        View view = inflater.inflate(R.layout.list_item, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
         return new ViewHolder(view);
     }
 
@@ -46,14 +45,45 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder>{
         return tasks.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         final ImageView picView;
         final TextView nameView, discView;
-        ViewHolder(View view){
+
+        ViewHolder(View view) {
             super(view);
+            view.setOnClickListener(this);
             picView = view.findViewById(R.id.pic);
             nameView = view.findViewById(R.id.name);
             discView = view.findViewById(R.id.disc);
         }
+
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(view.getContext(), "position = " + getLayoutPosition(), Toast.LENGTH_SHORT).show();
+
+            //go through each item if you have few items within recycler view
+            if (getLayoutPosition() == 0) {
+                //Do whatever you want here
+
+            } else if (getLayoutPosition() == 1) {
+                //Do whatever you want here
+
+            } else if (getLayoutPosition() == 2) {
+
+            } else if (getLayoutPosition() == 3) {
+
+            } else if (getLayoutPosition() == 4) {
+
+            } else if (getLayoutPosition() == 5) {
+
+            }/*
+
+            //or you can use For loop if you have long list of items. Use its length or size of the list as
+            for(int i = 0; i < exampleList.size(); i++){
+
+            }*/
+        }
+
     }
 }
+
